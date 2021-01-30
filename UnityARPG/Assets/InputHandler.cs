fe0917ddbb35bb05635b9b45d03142d47cf2f1cb
charlsdm <charlsdm@foxmail.com>
2021-01-30 17:04:08 +0800

    Unity

---
+++ UnityARPG/Assets/InputHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public float horizontal;
+    public float vertical;
+    public float moveAmount;
+    public float mouseX;
+    public float mouseY;
+    //PlayerControls inputActions;
+    Vector2 movementInput;
+    Vector2 cameraInput;
+    public void OnEnable()
+    {
+       // if(inputActions==null)
+        {
+           // inputActions = new PlayerControls();
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
