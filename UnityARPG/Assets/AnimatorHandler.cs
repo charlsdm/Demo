4a3c7ba7d28b00042e5d234a6b7ebb188baf4617
charlsdm <charlsdm@foxmail.com>
2021-02-03 11:34:09 +0800

    提交部分新的代码

---
+++ UnityARPG/Assets/AnimatorHandler.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorHandler : MonoBehaviour
+{
+
+    public Animator anim;
+    int vertical;
+    int horizontal;
+    public bool canRotate;
+    // Start is called before the first frame update
+
+    public void Init()
+    {
+        anim = GetComponent<Animator>();
+        vertical = Animator.StringToHash("Vertical");
+        horizontal = Animator.StringToHash("Horizontal");
+
+    }
+
+    public void UpdateAnimatorValues(float verticalMovement,float horizontalMovement)
+    {
+        #region Vertical;
+        float v = 0;
+        if(verticalMovement>0&&verticalMovement<0.55f)
+        {
+            v = 0.5f;
+        }
+        else if(verticalMovement>0.55f)
+        {
+            v = 1.0f;
+        }
+        else if(verticalMovement<0&&verticalMovement>=-0.55f)
+        {
+            v = -0.5f;
+        }
+        else if(verticalMovement<-0.55f)
+        {
+            v = -1f;
+        }
+        else
+        {
+            v = 0;
+        }
+        #endregion
+
+        #region Horizontal
+        float h = 0;
+        if(horizontalMovement>0&&horizontalMovement<0.55f)
+        {
+            h = 0.5f;
+        }
+        else if(horizontalMovement>0.55f)
+        {
+            h = 1.0f;
+        }
+        else if(horizontal<0&&horizontal>=-0.55f)
+        {
+            h = -0.5f;
+        }
+        else if(horizontal<-0.55f)
+        {
+            h = -1;
+        }
+        else
+        {
+            h = 0;
+        }
+        #endregion
+
+        anim.SetFloat(vertical, v, 0.1f, Time.deltaTime);
+        anim.SetFloat(horizontal, h, 0.1f, Time.deltaTime);
+    }
+
+    public void  CanRotate()
+    {
+        canRotate = true;
+    }
+    public void  StopRotation()
+    {
+        canRotate = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- UnityARPG/Assets/PlayerLocamotion.cs
+++ UnityARPG/Assets/PlayerLocamotion.cs
@@ -8,14 +8,69 @@ public class PlayerLocamotion : MonoBehaviour
     InputHandler inputHandler;
     Vector3 moveDirection;
     // Start is called before the first frame update
+    [HideInInspector]
+    public Transform myTransform;
+    [HideInInspector]
+    public AnimatorHandler animatorHandler;
+
+    public new Rigidbody rigidbody;
+    public GameObject normalCamera;
+
+    [Header("Stats")]
+    [SerializeField]
+    float movementSpeed = 5;
+    [SerializeField]
+    float rotationSpeed = 10;
+
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        inputHandler = GetComponent<InputHandler>();
+        animatorHandler = GetComponentInChildren<AnimatorHandler>();
+        cameraObject = Camera.main.transform;
+        myTransform = transform;
+        animatorHandler.Init();
     }
+    #region Movement
+    Vector3 normalVector;
+    Vector3 targetPosition;
 
-    // Update is called once per frame
-    void Update()
+    private void HandleRotation(float delta)
     {
-        
+        Vector3 targetDir = Vector3.zero;
+        float moveOverride = inputHandler.moveAmount;
+        targetDir = cameraObject.forward * inputHandler.vertical;
+        targetDir += cameraObject.right * inputHandler.horizontal;
+        targetDir.Normalize();
+        targetDir.y = 0;
+
+        if (targetDir == Vector3.zero)
+            targetDir = myTransform.forward;
+        float rs = rotationSpeed;
+        Quaternion tr = Quaternion.LookRotation(targetDir);
+        Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+        myTransform.rotation = targetRotation;
+
+    }
+    #endregion
+    // Update is called once per frame
+    public void Update()
+    {
+        float delta = Time.deltaTime;
+        inputHandler.TickInput(delta);
+        moveDirection = cameraObject.forward * inputHandler.vertical;
+        moveDirection += cameraObject.right * inputHandler.horizontal;
+        moveDirection.Normalize();
+
+        float speed = movementSpeed;
+        moveDirection *= speed;
+
+        Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+        rigidbody.velocity = projectedVelocity;
+        if(animatorHandler.canRotate)
+        {
+            HandleRotation(delta);
+        }
     }
 }
